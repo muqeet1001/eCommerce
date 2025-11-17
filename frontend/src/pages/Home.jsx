d71cacd107cafc6a7b61722c463c3597aebcdf1f
muqeet1001 <abdul1002muqeet@gmail.com>
2025-11-17 23:11:54 +0530

    Implement routing and update page components for eCommerce application

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='bg-amber-300'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, consectetur? Dolor distinctio mollitia autem, dolore officia debitis ipsam, facilis, quaerat nobis et provident possimus aspernatur consectetur perferendis eligendi cumque fuga!</div>
   )
 }
 

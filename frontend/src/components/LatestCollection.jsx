a83b5eff95858881cf41468d2bf03584a4e6a21e
muqeet1001 <abdul1002muqeet@gmail.com>
2025-11-20 00:36:45 +0530

    Add Title component and integrate it into LatestCollection for improved layout

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { ShopContext } from '../context/ShopContext.jsx'
+import Title from './Title.jsx'
 function LatestCollection() {
     const {products} = React.useContext(ShopContext);
-    console.log(products);
     
   return (
-    <div>
+    <div className='my-10'>
+        <div className='text-center py-8 text-3xl'>
+            <Title text1={'LATEST'} text2={'COLLECTION'}/>
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600 '>
+                Discover our latest collection of trendy and stylish products, carefully curated to keep you ahead in fashion. From chic apparel to must-have accessories, explore the newest arrivals that blend contemporary designs with exceptional quality. 
+            </p>
+        </div>
       
     </div>
   )

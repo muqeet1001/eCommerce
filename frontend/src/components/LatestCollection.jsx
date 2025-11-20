bf20e7a493509def9a69f32ca8a891e72580f8aa
muqeet1001 <abdul1002muqeet@gmail.com>
2025-11-20 13:22:51 +0530

    small changes is p

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -14,7 +14,7 @@ function LatestCollection() {
       <div className='text-center py-8 text-3xl'>
         <Title text1={'LATEST'} text2={'COLLECTION'} />
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600 '>
-          Discover our latest collection of trendy and stylish products, carefully curated to keep you ahead in fashion. From chic apparel to must-have accessories, explore the newest arrivals that blend contemporary designs with exceptional quality.
+          Discover our latest collection of trendy and stylish products. From chic apparel to must-have accessories, explore the newest arrivals that blend contemporary designs with exceptional quality.
         </p>
       </div>
       <div  className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 '>

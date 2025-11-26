2d0c678ea14b63252c57bfed3c7a702aa6b8a5d9
muqeet1001 <abdul1002muqeet@gmail.com>
2025-11-26 16:11:38 +0530

    home page done

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 function Footer() {
   return (
-    <div className='my-10'>email</div>
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+       <div>
+        <img className='mb-5 w-32' src={assets.logo} alt="" />
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+       </div>
+       <div>
+        <p className='text-xl font-medium mb-5'>COMPANY</p>
+        <ul className='flex flex-col gap-1 text-gray-600'>
+          <li>Home</li>
+          <li>Collection</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+       </div>
+       <div>
+        <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+         <ul className='flex flex-col gap-1 text-gray-600'>
+          <li>+91-9945037962</li>
+          <li>abdul00muqeet@gmail.com</li>
+         </ul>
+       </div>
+      </div>
+      <div>
+      <hr className='border-gray-300' />
+      <p className='text-center text-gray-600 text-m py-5'>&copy; 2025 Muqeet. All rights reserved.</p>
+      </div>
+    </div>
   )
 }
 

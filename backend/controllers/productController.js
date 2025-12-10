a69ebf3057fc00dbb0a1b72a5b8b8f5a06e0f30b
muqeet1001 <abdul1002muqeet@gmail.com>
2025-12-10 22:39:36 +0530

    multer

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,9 +1,21 @@
 
 //function for add product
 const addProduct = async (req, res) => {
+     try{
+        const {name,description,price,category,size,bestselelr} = req.body;
+        
+        const image1 = req.file.image1[0];
+        const image2 = req.file.image2[0];
+        const image3 = req.file.image3[0];
+        const image4 = req.file.image4[0];
 
+        console.log(name,description,price,category,size,bestselelr);
+        res.json({})
+     }
+     catch (e){
+         console.log(e);
+     }
 }
-
 //function for list product
 const listProduct = async (req, res) => {
 
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import {addProduct,removeProduct,singleProduct,listProduct} from '../controllers/productController.js'
- 
+import upload from './../middleware/multer';
 
 const ProductRouter = express.Router();
 
-ProductRouter.post('/add',addProduct);
+ProductRouter.post('/add',upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1}]),addProduct);
 ProductRouter.post('/remove',removeProduct);
-ProductRouter.post('/singe',singleProduct);
+ProductRouter.post('/single',singleProduct);
 ProductRouter.get('/list',listProduct);
 
 export default ProductRouter;
